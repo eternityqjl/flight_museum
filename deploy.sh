85bacf795bd0ac6bcd8f723c07382811b5a336f9
eternityqjl <eternityqjl@gmail.com>
2019-10-19 14:32:13 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!bin/sh
 git checkout master
-
 git add .
 git commit -m "Update"
 git push -u origin master
+
 gitbook build
 
-
 git checkout gh-pages
+
 cp -r _book/* . 
+
 git add .
 git commit -m "Update"
 git push -u origin gh-pages
